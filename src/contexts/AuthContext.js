513f8b76f6d4885ab3dd8476f89a257ee2226f25
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2021-09-01 17:15:35 -0300

    rotas

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,22 +1,47 @@
 import { createContext, useState } from "react";
+import { Redirect } from "react-router-dom";
 
 import firebase from "../lib/firebase";
 
 const AuthContext = createContext();
 
+const formatUser = async (user) => ({
+  uid: user.uid,
+  email: user.email,
+  name: user.displayName,
+  token: user.za,
+  photoUrl: user.photoURL,
+});
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
-  console.log(user)
+
+  if (user) {
+  }
+
+  console.log(user);
+
+  const handleUser = async (currentUser) => {
+    if (currentUser) {
+      const formatedUser = await formatUser(currentUser);
+      setUser(formatedUser);
+      return formatedUser.email;
+    }
+    setUser(false);
+    return false;
+  };
 
   const signinGoogle = async () => {
     try {
       const response = await firebase
         .auth()
         .signInWithPopup(new firebase.auth.GoogleAuthProvider());
-        setUser(response)
+      handleUser(response.user);
     } catch {
-      return;  
+      return;
+    } finally {
+      <Redirect to="/chat" />;
+
     }
   };
 
---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { Login } from "./pages/Login";
+
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={<Login />} />
+    </Switch>
+  </BrowserRouter>
+)
