36d0f55ee3c27fff22934a0231b36eecb9dcf0e4
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2021-09-04 19:42:46 -0300

    variavel ambiente

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,13 +4,13 @@ import "firebase/database";
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
-    apiKey: "AIzaSyD4blrRzIgoHKXboNw_TfV5Hzr1TIc-muU",
-    authDomain: "chat-box-50682.firebaseapp.com",
-    databaseURL: "https://chat-box-50682-default-rtdb.firebaseio.com",
-    projectId: "chat-box-50682",
-    storageBucket: "chat-box-50682.appspot.com",
-    messagingSenderId: "412858819627",
-    appId: "1:412858819627:web:e78a2eccdfd99e0d5fb66e",
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
   });
 }
 
